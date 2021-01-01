3f406f6aa147bc7e334d81a357e72726469c99e4
Hyper A'Hern <wl295462@dal.ca>
2021-01-01 18:43:45 -0400

    initial commit

---
+++ Assets/Scripts/ThrowObject.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowObject : MonoBehaviour
+{
+
+    public GameObject projectile;
+    public AudioClip shootSound;
+
+
+    private float throwSpeed = 2000f;
+    private AudioSource source;
+    private float volLowRange = .5f;
+    private float volHighRange = 1.0f;
+
+
+    void Awake()
+    {
+
+        source = GetComponent<AudioSource>();
+
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            float vol = Random.Range(volLowRange, volHighRange);
+            source.PlayOneShot(shootSound, vol);
+            GameObject throwThis = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+            throwThis.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, throwSpeed));
+        }
+
+    }
+}
